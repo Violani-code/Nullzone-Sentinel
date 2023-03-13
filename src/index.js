811a60fe77f713bed2cbb0199ed695b803a497a2
raphaelwanders <violani.content@gmail.com>
2023-03-13 02:02:27 +0100

    base code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`Welcome back! The application <${c.user.tag}> has launched.\nThis application is owned and hosted by Violani#2494`);
+});
+
+client.login(process.env.TOKEN);
