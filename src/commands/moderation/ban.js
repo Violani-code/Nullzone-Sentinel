f545081d19c52c62304c8c399d3acb1d53da0fa7
raphaelwanders <violani.content@gmail.com>
2023-03-16 17:21:42 +0100

    Created /ban command

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,6 +1,60 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
+    callback: async (client, interaction) => {
+        const targetUserId = interaction.options.get('target-user').value;
+        const reason = interaction.options.get('reason')?.value || "no reason provided";
+        
+        await interaction.deferReply();
+
+        const targetUser = await interaction.guild.members.fetch(targetUserId);
+
+        if (!targetUser) {
+            await interaction.editReply(`That user doesn't exist.`);
+            return;
+        }
+
+        if (targetUser.id === interaction.guild.ownerId) {
+            await interaction.editReply(`You can't ban the server owner`);
+            return;
+        }
+
+        const targetUserRolePosition = targetUser.roles.highest.position;
+        const requestUserRolePosition = interaction.member.roles.highest.position;
+        const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+        if (interaction.member.id === interaction.guild.ownerId) {
+            try {
+                await targetUser.ban({ reason });
+                await interaction.editReply(
+                    `User ${targetUser} has been banned from ${interaction.guild.name}`
+                )
+            } catch (error) {
+                console.log(`There was an error (ban.js): ${error}`)
+            }
+            return;
+        }
+
+        if (targetUserRolePosition >= requestUserRolePosition) {
+            await interaction.editReply(`You can't ban someone with an equal or higher rank than you.`);
+            return;
+        }
+
+        if (targetUserRolePosition >= botRolePosition) {
+            await interaction.editReply(`I can't ban this user because they have an equal or higher rank than me.`);
+            return;
+        }
+
+        try {
+            await targetUser.ban({ reason });
+            await interaction.editReply(
+                `User ${targetUser} has been banned from ${interaction.guild.name}`
+            )
+        } catch (error) {
+            console.log(`There was an error (ban.js): ${error}`)
+        }
+    },
+
     name: 'ban',
     description: 'Bans specified member',
     options: [
@@ -21,10 +75,6 @@ module.exports = {
             type: ApplicationCommandOptionType.Integer,
         },
     ],
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
-
-    callback: (client, interaction) => {
-        interaction.reply(`ban..`);
-    }
+    permissionsRequired: [PermissionFlagsBits.BanMembers],
+    botPermissions: [PermissionFlagsBits.BanMembers],
 }
