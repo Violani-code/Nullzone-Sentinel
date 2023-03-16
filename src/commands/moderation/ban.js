f3221fc039d39c8368257d2bfaea841405f018b5
raphaelwanders <violani.content@gmail.com>
2023-03-16 01:09:56 +0100

    Command/Event handler

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -17,6 +17,7 @@ module.exports = {
         },
     ],
     permissionsRequired: [PermissionFlagsBits.Administrator],
+    botPermissions: [PermissionFlagsBits.Administrator],
 
     callback: (client, interaction) => {
         interaction.reply(`ban..`);
--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -1,5 +1,62 @@
-const {devs} = require('../../../config.json');
+const { devs, testServer } = require('../../../config.json');
+const getLocalCommands = require('../../utils/getLocalCommands');
 
-module.exports = (client, interaction) => {
 
+module.exports = async (client, interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const localCommands = getLocalCommands();
+
+    try {
+        const commandObject = localCommands.find((cmd) => cmd.name === interaction.commandName);
+        
+        if (!commandObject) return;
+
+        if (commandObject.devOnly) {
+            if (!devs.includes(interaction.member.id)) {
+                interaction.reply({
+                    content: 'Only the developers are currently allowed to use this command',
+                    ephemeral: true,
+                })
+                return;
+            }
+        }
+        if (commandObject.testOnly) {
+            if (!(interaction.guild.id === testServer)) {
+                interaction.reply({
+                    content: 'This command is currently in its testing phase',
+                    ephemeral: true,
+                })
+                return;
+            }
+        }
+
+        if (commandObject.permissionsRequired?.length) {
+            for (const permission of commandObject.permissionsRequired) {
+                if (!interaction.member.permission.has(permission)) {
+                    interaction.reply({
+                        content: 'You do not have the required permissions',
+                        ephemeral: true,
+                    })
+                    return;
+                }
+            }
+        }
+        if (commandObject.botPermissions?.lenght) {
+            for (const permission of commandObject.botPermissions) {
+                const bot = interaction.guild.members.me;
+
+                if (!botPermissions.has(permission)) {
+                    interaction.reply({
+                        content: 'The bot has insufficient permissions',
+                        ephemeral: true,
+                    })
+                    return;
+                }
+            }
+        }
+        await commandObject.callback(client, interaction);
+    } catch (error) {
+        console.log(`There was an error (handleCommands): ${error}`)
+    }
 }
