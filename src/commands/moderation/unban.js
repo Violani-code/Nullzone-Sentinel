cba763cd88559bc665d4e5461900e9f9468d5300
raphaelwanders <violani.content@gmail.com>
2023-03-16 22:17:50 +0100

    Finished first version of Unban

--- src/commands/moderation/unban.js
+++ src/commands/moderation/unban.js
@@ -1,8 +1,40 @@
 module.exports = {
     callback: (client, interaction) => {
-        
+        const targetUserId = interaction.options.get('target-user').value;
+
+        await interaction.deferReply();
+
+        const targetUser = await interaction.guild.bans.fetch(targetUserId);
+
+        if (!targetUser) {
+            await interaction.editReply(`That user doesn't exist.`);
+            return;
+        }
+
+        const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+        try {
+            // console.log(targetUserId);
+            await guild.bans.remove(targetUserId);
+            await interaction.editReply({
+                content: `User ${targetUser} has been unbanned from **${interaction.guild.name}**`,
+                ephemeral: true,
+            });
+        } catch (error) {
+            console.log(`There was an error (unban.js): ${error}`);
+        }
     },
 
     name: `unban`,
     description: `Unbans specified member`
+    options: [
+        {
+            name: `target-user`,
+            description: `The user you wish to unban.`,
+            required: true,
+            type: ApplicationCommandOptionType.Mentionable
+        },
+        permissionsRequired: [PermissionFlagsBits.BanMembers],
+        botPermissions: [PermissionFlagsBits.BanMembers],
+    ],
 }
